35cf0ba0f836147cfb75683ccc64bac84f246932
TornadoTebbe <paul.tebbe@yahoo.com>
2022-12-06 17:00:20 +0100

    Erstellen Scatterplot Show
     um eventuell Scatterplot darstellen zu könne

--- src/ScatteplotDaten.elm
+++ src/ScatteplotDaten.elm
@@ -1,37 +1,33 @@
-module Scatterplot exposing (..)
+module ScatteplotDaten exposing (..)
 
 import Axis
+import Browser
 import Html exposing (..)
-import Html.Attributes exposing (value, href)
-import Html.Events exposing (onInput)
+import Html.Attributes
+import Html.Events
 import Scale exposing (ContinuousScale)
-import Shape exposing (..)
 import Statistics
-import TypedSvg exposing (circle, g, style, svg, text_)
-import TypedSvg.Attributes exposing (class, fontFamily, fontSize, textAnchor, transform, viewBox)
-import TypedSvg.Attributes.InPx exposing (cx, cy, r, x, y)
+import TypedSvg exposing (circle, g, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, color, fontFamily, fontSize, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, y)
 import TypedSvg.Core exposing (Svg)
-import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
-import Csv.Decode exposing (..)
-import Csv exposing (Csv)
-import TypedSvg.Attributes exposing (points)
-import Main exposing (Student_Data)
+import TypedSvg.Events
+import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Transform(..))
+import Csv
+import Csv.Decode
+import Http
 
 
--- type StudentAttributes 
---     = salaryExpectation
---     | tenthMark
---     | twelthMark
---     | collegeMark
-
-
-type alias Model =
-    { studentTribute1 : StudentAttributes
-    , studentTribute2 : StudentAttributes
-    }
+type Msg
+    = GotText (Result Http.Error String)
+    -- | ChooseAttribute1 String
+    -- | ChooseAttribute2 String
+    -- | ShowText (Svg Msg)
+    -- | DisableText
+    -- | Error String
 
 type alias Point =
-    { pointName : String, x : Float, y : Float }
+    { pointName : String, salaryExpectation : Int, tenthMark : Float, twelthMark : Float, collegeMark : Float}
 
 
 type alias XyData =
@@ -39,6 +35,18 @@ type alias XyData =
     , yDescription : String
     , data : List Point
     }
+
+type Model
+  = Failure
+  | Loading
+  | Success 
+        {data: List Student_Data
+     , position: String
+    --  , x: StudentAttributes
+    --  , y: StudentAttributes}
+        }
+
+
 
 type alias Student_Data =
     {   certification : String
@@ -62,6 +70,24 @@ type alias Student_Data =
     ,   partTimeJob : String --Bool
     }
 
+
+type alias Configuration =
+    { data: List Student_Data
+    , description: String
+    }
+
+
+-- type StudentAttributes 
+--     = Points
+--     | salaryExpectation
+--     | tenthMark
+--     | twelthMark
+--     | collegeMark
+ 
+
+
+
+-- Scatterplot
 
 
 w : Float
@@ -148,8 +174,8 @@ yAxis values =
 
 --scatterplot
 
-scatterplot : XyData -> Svg msg
-scatterplot model =
+scatterplot : Model -> XyData -> Svg Msg
+scatterplot model xyData =
     let
         --Testpunkt
         kreisbeschriftung : String
@@ -252,6 +278,10 @@ point scaleX scaleY xyPoint =
         ]
 
 
+
+studentToPoint : 
+
+
 andMap : Maybe a -> Maybe (a -> b) -> Maybe b
 andMap =
   Maybe.map2 (|>)
@@ -271,42 +301,169 @@ map4 function maybe1 maybe2 maybe3 maybe4 =
     |> andMap maybe4
 
 
-stringtoMaybeStudent : String -> Maybe StudentAttribute
-stringtoMaybeStudent str = 
-    case str of
-        "salary expectation" ->
 
 
 
--- studentToPoint : Student_Data -> Model -> Maybe Point
--- studentToPoint student =
---     let
-        
---     in
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Laden der Daten
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+daten : List String
+daten = [ "Student_Behaviour"]
+
+
+
+--hochladen der Daten aus dem Github
+init : () -> ( Model, Cmd Msg )
+init _ =
+    (Loading , 
+    Http.get
+    { url = "https://raw.githubusercontent.com/TornadoTebbe/ElmTest/main/Daten/Student_Behaviour.csv"
+    , expect = Http.expectString GotText
+    })
+    
+-- UPDATE
+
+--Decodierung der Daten:
+
+csvString_to_data : String -> List Student_Data
+csvString_to_data csvRaw =
+    Csv.parse csvRaw
+        |> Csv.Decode.decodeCsv decodeCsvStudentdata
+        |> Result.toMaybe
+        |> Maybe.withDefault []
+
+
+decodeCsvStudentdata : Csv.Decode.Decoder (Student_Data -> a ) a 
+decodeCsvStudentdata =
+    Csv.Decode.map Student_Data
+        (Csv.Decode.field "Certification Course" Ok
+              |> Csv.Decode.andMap (Csv.Decode.field "Gender" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "Department" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "Height(CM)"(String.toFloat >> Result.fromMaybe "error parsing string"))
+              |> Csv.Decode.andMap (Csv.Decode.field "Weight(KG)" (String.toFloat >> Result.fromMaybe "error parsing string"))
+              |> Csv.Decode.andMap (Csv.Decode.field "10th Mark" (String.toFloat >> Result.fromMaybe "error parsing string"))
+              |> Csv.Decode.andMap (Csv.Decode.field "12th Mark" (String.toFloat >> Result.fromMaybe "error parsing string"))
+              |> Csv.Decode.andMap (Csv.Decode.field "college mark" (String.toFloat >> Result.fromMaybe "error parsing string"))
+              |> Csv.Decode.andMap (Csv.Decode.field "hobbies" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "daily studing time" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "prefer to study in" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "salary expectation" (String.toInt >> Result.fromMaybe "error parsing string"))
+              |> Csv.Decode.andMap (Csv.Decode.field "Do you like your degree?" Ok) --(String.toBool >> Result.fromMaybe "error parsing string")) 
+              |> Csv.Decode.andMap (Csv.Decode.field "willingness to pursue a career based on their degree  " Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "social medai & video" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "Travelling Time " Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "Stress Level " Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "Financial Status" Ok)
+              |> Csv.Decode.andMap (Csv.Decode.field "part-time job" Ok) --(String.tool >> Result.fromMaybe "error parsing string")) 
+        )
+
+
+studentListe : List String -> List Student_Data
+studentListe student_liste =
+    List.map(\x -> csvString_to_data x) student_liste
+        |> List.concat
     
 
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                   (Success <| { data = csvString_to_data fullText, description = fullText }, Cmd.none)
+
+                Err _ ->
+                  {--( { model | status = Failure }, Cmd.none )--}  
+                  (Failure, Cmd.none)
+
+        {--BiggestWin ->
+          ( Success <| { m | description= "Whatever stuff"}, Cmd.none ) --}
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
 
+displaytext : Configuration -> Html Msg
+displaytext conf =
+    let
+        var = String.fromInt (List.length conf.data)
+    in
+         pre [] [ text (var)
+         , text (conf.description)]
+        --  , Html.button [ onClick BiggestWin ] [ text "Click me" ] ]
+    
+        
 
--- filterReduceStudents : List Student_Data -> XyData
--- filterReduceStudents newStudents = 
---     let 
---         filterStudents
---             List.filterMap studentToPoint newStudents
---     in
---     XyData "Salary Expectation" "Tenth Mark" filterStudents
+view : Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "I was unable to load your data."
 
+        Loading ->
+            text "Loading..."
 
--- filterData : List Student_Data -> String -> List Student_Data
--- filterData dataDone position =
---     List.filter (\c -> c.pos == position) dataDone
-
-
-
-
-
-
+        Success fullText ->
+            displaytext fullText
